c2bcae5b726eb30746c18613259739a024e344b2
Michael Zelenyuk <zell.xp@gmail.com>
2015-03-08 13:56:07 +0200

    Add filters and routes

--- js/app.js
+++ js/app.js
@@ -1,70 +1,135 @@
-(function ($) {
+﻿(function ($) {
 
     var contacts = [
-        { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD",
-            tel: "0123456789", email: "anemail@me.com", type: "family" },
-        { name: "Contact 2", address: "1, a street, a town, a city, AB12 3CD",
-            tel: "0123456789", email: "anemail@me.com", type: "family" },
-        { name: "Contact 3", address: "1, a street, a town, a city, AB12 3CD",
-            tel: "0123456789", email: "anemail@me.com", type: "friend" },
-        { name: "Contact 4", address: "1, a street, a town, a city, AB12 3CD",
-            tel: "0123456789", email: "anemail@me.com", type: "colleague" },
-        { name: "Contact 5", address: "1, a street, a town, a city, AB12 3CD",
-            tel: "0123456789", email: "anemail@me.com", type: "family" },
-        { name: "Contact 6", address: "1, a street, a town, a city, AB12 3CD",
-            tel: "0123456789", email: "anemail@me.com", type: "colleague" },
-        { name: "Contact 7", address: "1, a street, a town, a city, AB12 3CD",
-            tel: "0123456789", email: "anemail@me.com", type: "friend" },
-        { name: "Contact 8", address: "1, a street, a town, a city, AB12 3CD",
-            tel: "0123456789", email: "anemail@me.com", type: "family" }
+        { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789",
+            email: "anemail@me.com", type: "Family" },
+        { name: "Contact 2", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789",
+            email: "anemail@me.com", type: "Family" },
+        { name: "Contact 3", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789",
+            email: "anemail@me.com", type: "Friend" },
+        { name: "Contact 4", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789",
+            email: "anemail@me.com", type: "Colleague" },
+        { name: "Contact 5", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789",
+            email: "anemail@me.com", type: "Family" },
+        { name: "Contact 6", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789",
+            email: "anemail@me.com", type: "Colleague" },
+        { name: "Contact 7", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789",
+            email: "anemail@me.com", type: "Friend" },
+        { name: "Contact 8", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789",
+            email: "anemail@me.com", type: "Family" }
     ];
 
-var Contact = Backbone.Model.extend({
-    defaults: {
-        photo: "img/placeholder.png"
-    }
-});
+    var Contact = Backbone.Model.extend({
+        defaults: {
+            photo: "img/placeholder.png"
+        }
+    });
 
-var Directory = Backbone.Collection.extend({
-    model: Contact
-});
+    var Directory = Backbone.Collection.extend({
+        model: Contact
+    });
 
-var ContactView = Backbone.View.extend({
-    tagName: "article",
-    className: "contact-container",
-    template: $("#contactTemplate").html(),
+    var ContactView = Backbone.View.extend({
+        tagName: "article",
+        className: "contact-container",
+        template: _.template($("#contactTemplate").html()),
 
-    render: function () {
-        var tmpl = _.template(this.template);
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
 
-        this.$el.html(tmpl(this.model.toJSON()));
-        return this;
-    }
-});
+    var DirectoryView = Backbone.View.extend({
+        el: $("#contacts"),
 
-var DirectoryView = Backbone.View.extend({
-    el: $("#contacts"),
+        initialize: function () {
+            this.collection = new Directory(contacts);
 
-    initialize: function () {
-        this.collection = new Directory(contacts);
-        this.render();
-    },
+            this.render();
+            this.$el.find("#filter").append(this.createSelect()); 
 
-    render: function () {
-        var that = this;
-        _.each(this.collection.models, function (item) {
-            that.renderContact(item);
-        }, this);
-    },
+            this.on("change:filterType", this.filterByType, this);
+            this.collection.on("reset", this.render, this);
+        },
 
-    renderContact: function (item) {
-        var contactView = new ContactView({
-            model: item
-        });
-        this.$el.append(contactView.render().el);
-    }
-});
+        render: function () {
+            this.$el.find("article").remove();
 
-var directory = new DirectoryView();
+            _.each(this.collection.models, function (item) {
+                this.renderContact(item);
+            }, this);
+        },
+
+        renderContact: function (item) {
+            var contactView = new ContactView({
+                model: item
+            });
+            this.$el.append(contactView.render().el);
+        },
+
+        getTypes: function () {
+            return _.uniq(this.collection.pluck("type"));
+        },
+
+        createSelect: function () {
+            var select = $("<select/>", {
+                    html: "<option value='all'>All</option>"
+                });
+
+            _.each(this.getTypes(), function (item) {
+                var option = $("<option/>", {
+                    value: item,
+                    text: item
+                }).appendTo(select);
+            });
+
+            return select;
+        },
+
+        events: {
+            "change #filter select": "setFilter"
+        },
+
+        setFilter: function (e) {
+            this.filterType = e.currentTarget.value;
+            this.trigger("change:filterType");
+        },
+
+        filterByType: function () {
+            if (this.filterType === "all") {
+                this.collection.reset(contacts);
+                contactsRouter.navigate("filter/all");
+            } else {
+                this.collection.reset(contacts, { silent: true });
+
+                var filterType = this.filterType,
+                    filtered = _.filter(this.collection.models, function (item) {
+                        return item.get("type") === filterType;
+                    });
+
+                this.collection.reset(filtered);
+
+                contactsRouter.navigate("filter/" + filterType);
+            }
+        }
+    });
+
+    var ContactsRouter = Backbone.Router.extend({
+        routes: {
+            "filter/:type": "urlFilter"
+        },
+
+        urlFilter: function (type) {
+            directory.filterType = type;
+            directory.trigger("change:filterType");
+        }
+    });
+
+    var directory = new DirectoryView();
+
+    var contactsRouter = new ContactsRouter();
+
+    Backbone.history.start();
 
 } (jQuery));
